c5110d36032d7ea70eba0a916d84040dfd5841f3
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-10-23 15:05:42 +0530

    tempLATe updated

--- server/controllers/datacontroller.js
+++ server/controllers/datacontroller.js
@@ -6,6 +6,14 @@ module.exports.AllData = async function (req, res) {
 	try {
 		// console.log(req.cookies);
 
+		let states = await States.find({})
+		// .sort('-createdAt')
+		// .populate({
+		// 	path: 'appliances',
+		// 	populate: {
+		// 		path: 'admin',
+		// 	},
+		// });
 		let appliances = await Appliances.find({})
 			.sort('-createdAt')
 			.populate('admin')
@@ -15,13 +23,20 @@ module.exports.AllData = async function (req, res) {
 					path: 'admin',
 				},
 			});
-		let users = await User.find({});
-		// console.log('---------users--------',users)
+		let users = await User.find({})
+		.sort('-createdAt')
+        .populate('appliances')
+        .populate({ 
+            path:'state',
+            
+        });
+		console.log('---------users--------',states)
 		// console.log('-----appliances---',appliances)
 		return res.render('data', {
 			title: 'Automation|DATA',
 			appliances: appliances,
 			all_users: users,
+			states:states
 		});
 	} catch (err) {
 		console.log('Error', err);
