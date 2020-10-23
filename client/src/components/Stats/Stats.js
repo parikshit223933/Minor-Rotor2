b599dd4b5277673adb64e71e50d41dca4b3cf9b1
parikshit223933 <pk223933@gmail.com>
2020-10-23 21:51:31 +0530

    update status designed

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -67,11 +67,107 @@ class Stats extends React.Component {
 						<div className="font-30 text-center status-headings mb-4">
 							Update Status
 						</div>
-						<div></div>
+						<div className="status-container">
+							{this.props.auth.appliances.map((appl, index) => {
+								return (
+									<div
+										className="appliance my-3"
+										key={`appliance-index-${index}`}
+									>
+										<div className="mb-3">
+											<b>{Object.keys(appl)[0]}</b>
+										</div>
+										{Object.keys(
+											appl[Object.keys(appl)[0]]
+										).map((property, index) => {
+											return (
+												<div
+													className="pl-3"
+													style={{
+														display: 'flex',
+														flexDirection: 'row',
+														justifyContent:
+															'space-between',
+														alignItems: 'center',
+													}}
+													key={`property-index-${index}`}
+												>
+													<div>{property}: </div>
+													{typeof appl[
+														Object.keys(appl)[0]
+													][property] ===
+													typeof true ? (
+														appl[
+															Object.keys(appl)[0]
+														][property] ===
+														false ? (
+															<label
+																className="switch"
+																style={{
+																	marginBottom: 0,
+																}}
+															>
+																<input
+																	type="checkbox"
+																	defaultChecked={
+																		false
+																	}
+																/>
+																<span className="slider round"></span>
+															</label>
+														) : (
+															<label
+																className="switch"
+																style={{
+																	marginBottom: 0,
+																}}
+															>
+																<input
+																	type="checkbox"
+																	defaultChecked={
+																		true
+																	}
+																/>
+																<span className="slider round"></span>
+															</label>
+														)
+													) : (
+														<div class="form-group mb-0">
+															<input
+																type="range"
+																class="form-control-range"
+																min={0}
+																max={100}
+																step={10}
+																defaultValue={
+																	appl[
+																		Object.keys(
+																			appl
+																		)[0]
+																	][property]
+																}
+																id="formControlRange"
+															/>
+														</div>
+													)}
+												</div>
+											);
+										})}
+									</div>
+								);
+							})}
+						</div>
 					</div>
 				</div>
 				<div className="row mt-5">
-					<div className="col-md-6 py-3 text-right intro-box offset-md-6 bg-light">
+					<div className="col-md-6 py-3 d-flex justify-content-between intro-box offset-md-6 bg-light">
+					<button
+							type="button"
+							className="btn btn-success"
+							onClick={() => this.props.dispatch(logOut())}
+						>
+							Update
+						</button>
 						<button
 							type="button"
 							className="btn btn-danger"
--- server/controllers/api/v1/state_controller.js
+++ server/controllers/api/v1/state_controller.js
@@ -43,79 +43,6 @@ module.exports.getStateNamesForAppliance = (req, res) => {
 	});
 };
 
-// change the state of the appliance for the requesting user
-// req.body => {email, appliance_name, new_isTurnedOn_state, new_speed(in case of fan), new_intensity(in case of a bulb)}
-module.exports.changeState = async (req, res) => {
-	const { email, appliance_name, ...new_states } = req.body;
-	if (!email || !appliance_name) {
-		return helper.response(
-			res,
-			StatusCodes.NOT_ACCEPTABLE,
-			false,
-			'No appliance selected!'
-		);
-	}
-	if (!appliance_constants.allAppliances.includes(appliance_name)) {
-		return helper.response(
-			res,
-			StatusCodes.NOT_ACCEPTABLE,
-			false,
-			'Selected appliance is not supported!'
-		);
-	}
-	const all_states_of_selected_appliance =
-		applianceToStateMapping[appliance_name];
-	const are_all_recieved_state_names_correct = Object.keys(new_states).map(
-		(new_state_name) => {
-			return all_states_of_selected_appliance.includes(new_state_name);
-		}
-	);
-	if (are_all_recieved_state_names_correct.includes(false)) {
-		return helper.response(
-			res,
-			StatusCodes.NOT_ACCEPTABLE,
-			false,
-			'One of the state names recieved are in the wrong format'
-		);
-	}
-	// finally we can update the state!
-	try {
-		let user = await User.findOne({ email: email });
-		let appliance = await Appliance.findOne({ name: appliance_name });
-		if (!user || !appliance) {
-			return helper.internalServerError(res);
-		}
-		let state = await State.findOne({
-			admin: user.id,
-			appliance: appliance.id,
-		});
-		if (!state) {
-			return helper.response(
-				res,
-				StatusCodes.BAD_REQUEST,
-				false,
-				'This state does not exist!'
-			);
-		}
-		all_states_of_selected_appliance.forEach((new_state_name) => {
-			state[new_state_name] = new_states[new_state_name];
-		});
-		await state.save();
-		return helper.response(
-			res,
-			StatusCodes.OK,
-			true,
-			'State changed successfully!',
-			all_states_of_selected_appliance.map((new_state_name) => {
-				return { new_state_name: state[new_state_name] };
-			})
-		);
-	} catch (error) {
-		console.log(chalk.redBright.bold(error));
-		return helper.internalServerError(res);
-	}
-};
-
 //req.body => {user_id, selected_appliances: []}
 module.exports.selectAppliances = async (req, res) => {
 	const { user_id, selected_appliances } = req.body;
@@ -264,6 +191,79 @@ module.exports.getAllApplianceStatesForThisUser = async (req, res) => {
 		});
 	} catch (error) {
 		console.log(chalk.redBright.bold('There was an error!'), error);
+		return helper.internalServerError(res);
+	}
+};
+
+// change the state of the appliance for the requesting user
+// req.body => {email, appliance_name, new_isTurnedOn_state, new_speed(in case of fan), new_intensity(in case of a bulb)}
+module.exports.changeState = async (req, res) => {
+	const { email, appliance_name, ...new_states } = req.body;
+	if (!email || !appliance_name) {
+		return helper.response(
+			res,
+			StatusCodes.NOT_ACCEPTABLE,
+			false,
+			'No appliance selected!'
+		);
+	}
+	if (!appliance_constants.allAppliances.includes(appliance_name)) {
+		return helper.response(
+			res,
+			StatusCodes.NOT_ACCEPTABLE,
+			false,
+			'Selected appliance is not supported!'
+		);
+	}
+	const all_states_of_selected_appliance =
+		applianceToStateMapping[appliance_name];
+	const are_all_recieved_state_names_correct = Object.keys(new_states).map(
+		(new_state_name) => {
+			return all_states_of_selected_appliance.includes(new_state_name);
+		}
+	);
+	if (are_all_recieved_state_names_correct.includes(false)) {
+		return helper.response(
+			res,
+			StatusCodes.NOT_ACCEPTABLE,
+			false,
+			'One of the state names recieved are in the wrong format'
+		);
+	}
+	// finally we can update the state!
+	try {
+		let user = await User.findOne({ email: email });
+		let appliance = await Appliance.findOne({ name: appliance_name });
+		if (!user || !appliance) {
+			return helper.internalServerError(res);
+		}
+		let state = await State.findOne({
+			admin: user.id,
+			appliance: appliance.id,
+		});
+		if (!state) {
+			return helper.response(
+				res,
+				StatusCodes.BAD_REQUEST,
+				false,
+				'This state does not exist!'
+			);
+		}
+		all_states_of_selected_appliance.forEach((new_state_name) => {
+			state[new_state_name] = new_states[new_state_name];
+		});
+		await state.save();
+		return helper.response(
+			res,
+			StatusCodes.OK,
+			true,
+			'State changed successfully!',
+			all_states_of_selected_appliance.map((new_state_name) => {
+				return { new_state_name: state[new_state_name] };
+			})
+		);
+	} catch (error) {
+		console.log(chalk.redBright.bold(error));
 		return helper.internalServerError(res);
 	}
 };
