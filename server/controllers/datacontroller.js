44718e9ecf90ce1ffd62b60d71255fa880bc81c1
parikshit223933 <pk223933@gmail.com>
2020-10-24 22:17:59 +0530

    minor change

--- server/controllers/datacontroller.js
+++ server/controllers/datacontroller.js
@@ -4,16 +4,7 @@ const Appliances = require('../models/Appliance');
 const constants = require('../constants/appliance_to_state_mapping')
 module.exports.AllData = async function (req, res) {
 	try {
-		// console.log(req.cookies);
-
 		let states = await States.find({})
-		// .sort('-createdAt')
-		// .populate({
-		// 	path: 'appliances',
-		// 	populate: {
-		// 		path: 'admin',
-		// 	},
-		// });
 		let appliances = await Appliances.find({})
 			.sort('-createdAt')
 			.populate('admin')
@@ -30,8 +21,6 @@ module.exports.AllData = async function (req, res) {
             path:'state',
             
         });
-		console.log('---------users--------',constants.FAN[0])
-		// console.log('-----appliances---',appliances)
 		return res.render('data', {
 			title: 'Automation|DATA',
 			appliances: appliances,
